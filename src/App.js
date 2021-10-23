aabeab14239b514612d1b49cc71f2e69fc35def8
Peder Smith <smith.peder@gmail.com>
2021-10-23 22:41:15 +0200

    Implement Message Loop

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import Header from "./components/Header";
 import Message from "./components/Message";
+import data from "./api";
 import "./App.css";
 
 const App = () => {
+  const messages = data.map((msg) => (
+    <Message
+      from={msg.from}
+      timestamp={msg.timestamp}
+      text={msg.text}
+      me={msg.me}
+    />
+  ));
+
   return (
     <div className="chat">
       <Header text="Chatroom" />
-      <Message
-        from={"Peder"}
-        text={"Er dere klare?"}
-        timestamp={"17:14:12"}
-        me={true}
-      />
-      <Message
-        from={"Ludvig"}
-        text={"JaJa, kommer nå"}
-        timestamp={"17:15:12"}
-        me={false}
-      />
-      <Message
-        from={"Magnus"}
-        text={"Rett rundt hjørnet"}
-        timestamp={"17:16:33"}
-        me={false}
-      />
-      <Message
-        from={"Peder"}
-        text={"Venter fortsatt..."}
-        timestamp={"17:17:45"}
-        me={true}
-      />
+      {messages}
     </div>
   );
 };
