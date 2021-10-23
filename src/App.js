734ed82200eaef81446c36e8f4271c43396d40c0
Peder Smith <smith.peder@gmail.com>
2021-10-23 19:49:36 +0200

    Implement Message

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import Header from "./components/Header";
+import Message from "./components/Message";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="chat">
       <Header text="Chatroom" />
-      <div className={"message message-right"}>
-        <span className="from">Peder</span>
-        <span className="text text-right">Er dere klare?</span>
-        <span className="timestamp">17:14:12</span>
-      </div>
-      <div className={"message message-left"}>
-        <span className="from">Ludvig</span>
-        <span className="text text-left">JaJa, kommer nå</span>
-        <span className="timestamp">17:15:12</span>
-      </div>
-      <div className={"message message-left"}>
-        <span className="from">Magnus</span>
-        <span className="text text-left">Rett rundt hjørnet</span>
-        <span className="timestamp">17:16:33</span>
-      </div>
-      <div className={"message message-right"}>
-        <span className="from">Peder</span>
-        <span className="text text-right">Venter fortsatt...</span>
-        <span className="timestamp">17:17:45</span>
-      </div>
+      <Message
+        from={"Peder"}
+        text={"Er dere klare?"}
+        timestamp={"17:14:12"}
+        me={true}
+      />
+      <Message
+        from={"Ludvig"}
+        text={"JaJa, kommer nå"}
+        timestamp={"17:15:12"}
+        me={false}
+      />
+      <Message
+        from={"Magnus"}
+        text={"Rett rundt hjørnet"}
+        timestamp={"17:16:33"}
+        me={false}
+      />
+      <Message
+        from={"Peder"}
+        text={"Venter fortsatt..."}
+        timestamp={"17:17:45"}
+        me={true}
+      />
     </div>
   );
 };
